ea3b11cf8fb5d874e4dc5ae4058204c1a666de70
Hawes Collier <37671636+hawes1010@users.noreply.github.com>
2020-06-26 13:34:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+# Default template for XBee MicroPython projects
+
+# Default template for XBee MicroPython projects
+import ujson
+import urllib
+import urequests
+import uftp
+import sys
+from datetime import datetime
+print("Hello AirNow, I'm coming for yo data")
+# API Parameters
+# Your API Key: C8A2A7B9-E1A6-4D9E-B89C-88CF3E47B877
+
+
+
+
+options = {}
+# easier to read parts liek this
+options["url"] = "https://airnowapi.org/aq/data/"
+options["start_date"] = "2014-09-23"
+options["start_hour_utc"] = "22"
+options["end_date"] = "2014-09-23"
+options["end_hour_utc"] = "23"
+options["parameters"] = "o3,pm25"
+options["bbox"] = "32.37,-104.261,32.39,-104.263" # minX,minY,maxX,maxY
+options["data_type"] = "a"
+options["format"] = "application/xml"
+options["ext"] = "xml"
+options["api_key"] = "C8A2A7B9-E1A6-4D9E-B89C-88CF3E47B877"
+
+REQUEST_URL = options["url"] \
+                  + "?startdate=" + options["start_date"] \
+                  + "t" + options["start_hour_utc"] \
+                  + "&enddate=" + options["end_date"] \
+                  + "t" + options["end_hour_utc"] \
+                  + "&parameters=" + options["parameters"] \
+                  + "&bbox=" + options["bbox"] \
+                  + "&datatype=" + options["data_type"] \
+                  + "&format=" + options["format"] \
+                  + "&api_key=" + options["api_key"]
+
+try:
+    # Request AirNowAPI data
+    # User's home directory.
+    # Perform the AirNow API data request
+    api_data = urllib.URLopener()
+    # api_data.(REQUEST_URL)
+
+    # Download complete
+    """ print
+    "Download URL: %s" % REQUEST_URL
+    print
+    "Download File: %s" % download_file
+    """
+except Exception as e:
+    print("Unable perform AirNowAPI request. %s" % e)
+    sys.exit(1)
+url = options
+def Get_AirNow():
+    print("Reading data from Airnow")
